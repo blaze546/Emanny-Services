d73c139044184d57214831badcbc9ee153346b7e
Brian Alexandre <blaze546@msn.com>
2025-05-09 21:06:47 -0400

    carouselsection

--- src/components/CarouselSection.jsx
+++ src/components/CarouselSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Carousel } from 'react-bootstrap';
+import './CarouselSection.css';
 
 const CarouselSection = () => {
   return (
--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import './TextBlock.css';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+AOS.init(); // Initialize AOS
 
 const TextBlock = () => {
   return (
-    <div className="text-center mb-4">
-      <h2 className="fw-bold custom-gold">About Emanny Services</h2>
+    <div className="text-block-container" data-aos="fade-up" data-aos-duration="800">
+      <h2 className="text-block-heading">About Emanny Services</h2>
     </div>
   );
 };
 
 export default TextBlock;
+
