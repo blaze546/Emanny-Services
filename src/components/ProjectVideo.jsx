0264115adaae6756d16cf26293a00e7c36be7d40
Brian Alexandre <blaze546@msn.com>
2025-05-09 22:54:14 -0400

    mobile video fix

--- src/components/ProjectVideo.jsx
+++ src/components/ProjectVideo.jsx
@@ -5,14 +5,14 @@ const ProjectVideo = ({ src, title }) => {
   return (
     <div className="project-video mb-4">
       <h5 className="text-center mb-2">{title}</h5>
-      <div className="ratio ratio-16x9 shadow rounded project-video-container">
+      <div className="video-container">
         <video
           src={src}
           controls
           autoPlay
           muted
           loop
-          className="w-100 h-100 object-fit-cover"
+          className="w-100 h-100 object-fit-cover video-element"
         />
       </div>
     </div>
