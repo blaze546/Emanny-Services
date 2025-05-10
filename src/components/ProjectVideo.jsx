d09a4d63fa50fb7173785d635000d9e96bd6d2f7
Brian Alexandre <blaze546@msn.com>
2025-05-09 22:47:37 -0400

    addedmobilecustomization

--- src/components/ProjectVideo.jsx
+++ src/components/ProjectVideo.jsx
@@ -5,13 +5,21 @@ const ProjectVideo = ({ src, title }) => {
   return (
     <div className="project-video mb-4">
       <h5 className="text-center mb-2">{title}</h5>
-      <div className="ratio ratio-16x9 shadow rounded">
-        <video src={src} controls autoPlay muted loop className="w-100 h-100 object-fit-cover" />
+      <div className="ratio ratio-16x9 shadow rounded project-video-container">
+        <video
+          src={src}
+          controls
+          autoPlay
+          muted
+          loop
+          className="w-100 h-100 object-fit-cover"
+        />
       </div>
     </div>
   );
 };
 
 export default ProjectVideo;
+
 
 
